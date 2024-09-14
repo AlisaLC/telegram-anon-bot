d210a9a1ebc7cb5713beb4836143181dc56dd15c
AlisaLC <ali_sal1381@yahoo.com>
2024-09-14 10:02:39 +0330

    added bot channel

--- bot/bot.py
+++ bot/bot.py
@@ -6,6 +6,7 @@ import os
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
+bot_channel_id = os.getenv("BOT_CHANNEL_ID")
 
 app = Client(
     "anon",
